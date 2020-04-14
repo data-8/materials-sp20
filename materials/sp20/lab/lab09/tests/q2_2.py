1a65a9604f47e1720c7e159c119acc399e53a185
Austen <austenzhu@berkeley.edu>
2020-04-14 09:47:50 -0700

    lab09

---
+++ materials/sp20/lab/lab09/tests/q2_2.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'q2_2',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> (18 + intercept*(-4)) / 201 >= 0.3
+          False
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
